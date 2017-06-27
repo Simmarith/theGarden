5caf9abc1da7f13e4a6ceefbfb19c42304a780d3
simmarith <menzeladrian@googlemail.com>
2017-06-27 15:46:24 +0200

    Basic vr setup complete

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityEditor.PlayerSettings.virtualRealitySupported = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
